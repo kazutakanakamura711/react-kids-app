b70ec71707347e2b91c07f8cff0c78c30763c341
kazutakanakamura711 <a711st777@gmail.com>
2024-03-25 02:25:24 +0900

    Use routePathType

--- src/routes/publicRoutes.tsx
+++ src/routes/publicRoutes.tsx
@@ -1,14 +1,18 @@
 import { HomePage } from '@/pages/user/home';
 import { TopPage } from '@/pages/common/top';
 import { PrivateRoutes } from './PrivateRoutes';
+import { RouteLabelType } from '@/types/routeLabel';
+import { RoutePathType } from '@/types/routePath.ts';
 
 export const publicRoutes = [
   {
-    path: '/',
+    label: RouteLabelType.Top,
+    path: RoutePathType.Top,
     element: <TopPage />,
   },
   {
-    path: '/home',
+    label: RouteLabelType.Home,
+    path: RoutePathType.Home,
     element: (
       <PrivateRoutes>
         <HomePage />
