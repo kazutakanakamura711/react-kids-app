c7dafec4461c931b731c92fbb0c544e972c48e71
kazutakanakamura711 <a711st777@gmail.com>
2024-03-25 02:27:31 +0900

    Fix headers and dividers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import { AppRoutes } from './routes/indext';
+import { AppRoutes } from './routes/index';
+import { Header } from './components/layouts/Header';
+import { MainLayout } from './components/layouts/MainLayout';
+import { useEffect, useState } from 'react';
+import { RoutePathType } from './types/routePath.ts';
+
+const AppContent = () => {
+  const location = useLocation();
+  const [isAuthPage, setIsAuthPage] = useState(false);
+
+  useEffect(() => {
+    const path = location.pathname;
+    setIsAuthPage(path === RoutePathType.Top);
+  }, [location]);
+
+  return (
+    <>
+      {!isAuthPage ? (
+        <>
+          <Header />
+          <MainLayout>
+            <AppRoutes />
+          </MainLayout>
+        </>
+      ) : (
+        <AppRoutes />
+      )}
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <Router>
         <ChakraProvider>
-          <AppRoutes />
+          <AppContent />
         </ChakraProvider>
       </Router>
     </>
